eb16b4daf9580c01f01bdb625e5dff19985c03fc
Akash Gupta <agbichpuriya@gmail.com>
2022-03-19 21:42:29 +0530

    Score and SEC filling fixed

--- src/components/folder/BreadCrumb.jsx
+++ src/components/folder/BreadCrumb.jsx
@@ -1,16 +1,26 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
-import { findFolderListInCurrentList } from './Util';
-
+const findFolderListInCurrentList = (foldername, foldersArr) => {
+  for (let folder of foldersArr) {
+    if (folder.name === foldername) return folder;
+  }
+  return {
+    name: 'root',
+    path: '',
+    children: [],
+    isFolder: true,
+    content: null
+  };
+};
 const BreadCrumb = ({ pathUrl, rootFolder, onCrumbClick }) => {
-  //  console.log(pathUrl)
+  //console.log(pathUrl);
+
   const pathArr = pathUrl.split('/');
   pathArr.splice(pathArr.length - 1, 1);
   let tempObj;
   let tempFolder = rootFolder.children.slice();
   //console.log(pathArr);
-  return !pathArr.length ? null : (
+  return !pathArr || !pathArr.length ? null : (
     <div className="  flex flex-row space-x-2 items-center text-md font-semibold">
       <div className=" bread-crumb-list flex flex-row space-x-2 items-center">
         {pathArr.map((name) => {
