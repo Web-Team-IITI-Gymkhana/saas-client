362c9c8784c134582c1d6c4076a6ef2b23f8d30c
Akash Gupta <agbichpuriya@gmail.com>
2022-03-19 18:11:41 +0530

    Connection of companies to charts

--- src/components/base/atoms/Searchbar.jsx
+++ src/components/base/atoms/Searchbar.jsx
@@ -19,7 +19,92 @@ export default function AnimatedMulti() {
 
   /*query getCompanyByCIK($cik: String!) {
           getCompanyByCIK(cik: $cik) {
+            id
             CompanyName
+            Address
+            FaxNumber
+            HoldingType
+            PhoneNumber
+            URL
+            IPODate
+            exchange
+            ticker
+            _10k {
+              id
+              DocURL
+              FilingDate
+              FilingForDate
+              features {
+                ARR
+                CashAndCashEquivalents
+                CostOfSales
+                GAAPRevenue
+                Goodwill
+                GrossProfit
+                GrossPropertyAndEquipment
+                MRR
+                MarketableSecurities
+                NetIncome
+                NetLoss
+                NonGAAPEarnings
+                OperatingIncome
+                PropertyAndEquipmentNet
+                RecurringRevenue
+                Revenues
+                SalesAndMarketing
+                SharesOutstanding
+                StockPrice
+                TotalAssets
+                TotalCurrentAssets
+                TotalCurrentLiabilities
+                TotalDebt
+                TotalEquity
+                TotalOperatingExpenses
+                TotalStockholdersEquity
+              }
+              sec_filing {
+                name
+                url
+              }
+            }
+            _10q {
+              id
+              DocURL
+              FilingDate
+              FilingForDate
+              features {
+                ARR
+                CashAndCashEquivalents
+                CostOfSales
+                GAAPRevenue
+                Goodwill
+                GrossProfit
+                GrossPropertyAndEquipment
+                MRR
+                MarketableSecurities
+                NetIncome
+                NetLoss
+                NonGAAPEarnings
+                OperatingIncome
+                PropertyAndEquipmentNet
+                RecurringRevenue
+                Revenues
+                SalesAndMarketing
+                SharesOutstanding
+                StockPrice
+                TotalAssets
+                TotalCurrentAssets
+                TotalCurrentLiabilities
+                TotalDebt
+                TotalEquity
+                TotalOperatingExpenses
+                TotalStockholdersEquity
+              }
+              sec_filing {
+                name
+                url
+              }
+            }
           }
   }*/
 
