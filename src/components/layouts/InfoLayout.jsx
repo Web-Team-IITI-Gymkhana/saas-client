a279b6ab4ef664845706cd88f2a29848731bd279
Akash Gupta <agbichpuriya@gmail.com>
2022-03-19 18:15:26 +0530

    Landing Page, SearchBar changes and Loading Demo

--- src/components/layouts/InfoLayout.jsx
+++ src/components/layouts/InfoLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {CircularProgress, Box, Typography} from '@mui/material';
+import { CircularProgress, Box, Typography } from '@mui/material';
 
 const InfoLayout = ({ data }) => {
   const [progress, setProgress] = React.useState(0);
@@ -16,7 +16,7 @@ const InfoLayout = ({ data }) => {
       clearInterval(timer);
     };
   }, []);
-  
+
   return (
     <div className="flex flex-col h-full w-4/6 bg-saas-main ml-2 rounded-xl drop-shadow-sm p-6 hover:drop-shadow-xl">
       <div className="flex flex-row justify-between items-center">
@@ -24,7 +24,7 @@ const InfoLayout = ({ data }) => {
           <div className="text-4xl text-saas-accent font-black">
             {data.CompanyName}
           </div>
-          {data.URL!="NaN" ? <div className="flex flex-row pt-1">
+          {data.URL != "NaN" ? <div className="flex flex-row pt-1">
             <div className='text-lg text-saas-primary'>
               <FontAwesomeIcon icon={'globe'} />
             </div>
@@ -34,66 +34,66 @@ const InfoLayout = ({ data }) => {
           </div> : null}
         </div>
         <div className='pr-10'>
-          {data.HoldingType!="NaN" ? <div className="text-lg font-extrabold flex flex-row">
+          {data.HoldingType != "NaN" ? <div className="text-lg font-extrabold flex flex-row">
             <div className="text-saas-accent">Holding :</div>
             <div className="text-saas-primary pl-1">{data.HoldingType}</div>
-          </div>:null}
-          {data.Ticker!="NaN" ? <div className="text-lg font-extrabold flex flex-row">
+          </div> : null}
+          {data.Ticker != "NaN" ? <div className="text-lg font-extrabold flex flex-row">
             <div className="text-saas-accent">Stock Ticker :</div>
             <div className="text-saas-primary pl-1">{data.Ticker}</div>
           </div> : null}
-          {data.Exchange!="NaN" ? <div className="text-lg font-extrabold flex flex-row">
+          {data.Exchange != "NaN" ? <div className="text-lg font-extrabold flex flex-row">
             <div className="text-saas-accent">Exchange :</div>
             <div className="text-saas-primary pl-1">{data.Exchange}</div>
-          </div>:null}
+          </div> : null}
         </div>
       </div>
       {data.IPODate != "NaN" ? <div className="flex flex-row pt-5">
         <div className="text-xl py-0 text-saas-accent font-bold">
-            IPO Date :
+          IPO Date :
         </div>
         <div className="text-lg py-0.5 text-saas-primary font-semibold pl-2">
-            { data.IPODate.split("T")[0] }
+          {data.IPODate.split("T")[0]}
         </div>
       </div> : null}
-      {data.Address!="NaN" ? <div className="flex flex-row pt-5">
+      {data.Address != "NaN" ? <div className="flex flex-row pt-5">
         <div className="pt-2.5">
           <FontAwesomeIcon icon={'building'} className="pr-3 text-4xl" />
         </div>
         <div>
-        <div className="text-lg py-0 text-saas-accent font-semibold">
+          <div className="text-lg py-0 text-saas-accent font-semibold">
             HeadQuarter Address
-        </div>
-        <div className="text-base py-0 text-saas-primary font-medium">
-            { data.Address }
+          </div>
+          <div className="text-base py-0 text-saas-primary font-medium">
+            {data.Address}
           </div>
         </div>
       </div> : null}
-      {data.PhoneNumber!="NaN" ? <div className="flex flex-row pt-5">
+      {data.PhoneNumber != "NaN" ? <div className="flex flex-row pt-5">
         <div className="pt-2.5">
           <FontAwesomeIcon icon={'phone'} className="pr-3 text-4xl" />
         </div>
         <div>
-        <div className="text-lg py-0 text-saas-accent font-semibold">
+          <div className="text-lg py-0 text-saas-accent font-semibold">
             Phone Number
-        </div>
+          </div>
           <div className="text-base py-0 text-saas-primary font-medium">
-            <a href={"tel:+"+data.PhoneNumber}>
+            <a href={"tel:+" + data.PhoneNumber}>
               +{data.PhoneNumber}
             </a>
           </div>
         </div>
       </div> : null}
-      {data.FaxNumber=="NaN" ? <div className="flex flex-row pt-5">
+      {data.FaxNumber != "NaN" ? <div className="flex flex-row pt-5">
         <div className="pt-2.5">
           <FontAwesomeIcon icon={'fax'} className="pr-3 text-4xl" />
         </div>
         <div>
-        <div className="text-lg py-0 text-saas-accent font-semibold">
+          <div className="text-lg py-0 text-saas-accent font-semibold">
             Fax Number
-        </div>
+          </div>
           <div className="text-base py-0 text-saas-primary font-medium">
-            <a href={"fax:"+ data.FaxNumber}>
+            <a href={"fax:" + data.FaxNumber}>
               + {data.FaxNumber}
             </a>
           </div>
@@ -101,30 +101,30 @@ const InfoLayout = ({ data }) => {
       </div> : null}
       <div className='flex flex-col pt-10'>
         <div className='text-2xl font-bold pl-2'>
-            Score
+          Score
         </div>
-      <div className='flex flex-row pt-10'>
-        <div>
-          <Box sx={{ position: 'relative', display: 'inline-flex' }}>
-              <CircularProgress variant="determinate" size={100} thickness={6} value={progress} style={{color: "orange"}}/>
-            <Box
-              sx={{
-                top: 0,
-                left: 0,
-                bottom: 0,
-                right: 0,
-                position: 'absolute',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
+        <div className='flex flex-row pt-10'>
+          <div>
+            <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+              <CircularProgress variant="determinate" size={100} thickness={6} value={progress} style={{ color: "orange" }} />
+              <Box
+                sx={{
+                  top: 0,
+                  left: 0,
+                  bottom: 0,
+                  right: 0,
+                  position: 'absolute',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
                 <div className='text-lg font-black'>
                   {progress}
                 </div>
+              </Box>
             </Box>
-          </Box>
-        </div>
+          </div>
         </div>
       </div>
     </div>
