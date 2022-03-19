a0a6757ebd8de4d24310073464e74293edb8cdb2
Akash Gupta <67226124+akashg3627@users.noreply.github.com>
2022-03-19 21:44:02 +0530

    Score and SEC filling fixed (#29)

--- src/components/base/molecules/sidebar/SidebarBottomItem.jsx
+++ src/components/base/molecules/sidebar/SidebarBottomItem.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import ScoreChart from '../Charts/ScoreChart';
 const SidebarBottomItem = ({ children, ...nativeProps }) => {
   return (
     <div
       {...nativeProps}
-      className="flex justify-center w-5/6 h-40 mb-8 bg-saas-primary drop-shadow-lg  rounded-2xl hover:scale-105 duration-150 text-main"
+      className="flex flex-row pt-2 justify-center w-full h-40 mb-8 "
     >
-      {children}
+      <div className="flex flex-row bg-slate-50 bg-opacity-20 drop-shadow-lg w-9/12 justify-center items-center rounded-2xl hover:scale-105 px-4 duration-150 text-main ">
+        <ScoreChart data={96} label="Score" width="100%" />
+      </div>
     </div>
   );
 };
