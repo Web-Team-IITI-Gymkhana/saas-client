331b6c560ffa91df01235d7d0f00d80f83c23993
pineapple45 <gomraanmol@gmail.com>
2022-03-19 17:18:48 +0530

    added error-boundry to searchbar | basic cache persistance

--- src/apollo/index.jsx
+++ src/apollo/index.jsx
@@ -1,8 +1,29 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { persistCache, LocalStorageWrapper } from 'apollo3-cache-persist';
 
+const cache = new InMemoryCache();
+
+/****  Do Not Remove the following basic client initialization ***/
+
+// const client = new ApolloClient({
+//   uri: 'https://saas-server-production.up.railway.app/graphql',
+//   cache: new InMemoryCache()
+// });
+
+/*** Basic Apollo Client cache persistance */
+
+// await before instantiating ApolloClient, else queries might run before the cache is persisted
+await persistCache({
+  cache,
+  storage: new LocalStorageWrapper(window.localStorage)
+});
+
+// Continue setting up Apollo as usual.
 const client = new ApolloClient({
   uri: 'https://saas-server-production.up.railway.app/graphql',
-  cache: new InMemoryCache()
+  cache: cache
 });
+
+/** Advanced Apollo Client Query Persistance */
 
 export default client;
